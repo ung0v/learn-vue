c0543eb78cbde0ad3b6df1eebb9d787b93fd6639
Vũ Quốc Vương <quoc.vuong@blockodyssey.io>
2024-11-04 15:00:19 +0700

    study: vue slot

--- src/main.js
+++ src/main.js
@@ -1,40 +1,11 @@
-// import './assets/main.css'
-// import './assets/style.css'
-
-let PlanComponent = {
-    template: '#plan-template',
-    props: {
-        name: { type: String, default: 'The Verge', required: true },
-        selected: {type: Boolean, default: false}
-    },
-    // methods: {
-    //     select() {
-    //         this.$emit("select", this.name)
-    //     }
-    // }
-}
-
-let PlanPickerComponent = {
-    components: { plan: PlanComponent },
-    template: '#plan-picker-template',
-    data() {
-        return {
-            plans: ["The Single", "The Curious", "The Addict"],
-            selectedPlan: null
-        }
-    },
-    methods: {
-        selectPlan(plan) {
-            console.log(plan)
-            this.selectedPlan = plan
-        }
-    }
-}
-
-
-Vue.createApp({
-    components: {
-        'plan-picker': PlanPickerComponent
-    }
+const app = Vue.createApp({
 })
-    .mount('#app')
+.component('todo-item', {
+  template: '#todo-item-template',
+  data(){
+    return {
+      done: false
+    }
+  }
+})
+.mount('#app')
