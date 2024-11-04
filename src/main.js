c5e2247e6755e1bd580690fb4fab08e4f67d3170
Vũ Quốc Vương <quoc.vuong@blockodyssey.io>
2024-11-04 16:23:27 +0700

    feat: build github profile

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-const app = Vue.createApp({
-})
-.component('todo-item', {
-  template: '#todo-item-template',
-  data(){
-    return {
-      done: false
-    }
-  }
-})
-.mount('#app')
+// Vue.createApp({
+
+// })
+// .component('page-layout', {
+// 	template: '#page-layout-template'
+// })
+// .mount('#app')
